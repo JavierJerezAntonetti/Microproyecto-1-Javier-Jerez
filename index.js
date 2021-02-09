f4be897e8129f1103be55d5114504145c111a184
JavierJerezAntonetti <61585930+JavierJerezAntonetti@users.noreply.github.com>
2021-02-09 16:31:49 +0100

    hero
    creación del slider de imagenes

---
+++ index.js
@@ -0,0 +1,49 @@
+
+// ----------- HERO ------------
+window.onload = function () {
+    // Variables
+    const IMAGENES = [
+        './images/html-code.jpg',
+        './images/guy-coding.jpg',
+        './images/laptop-code.jpg'
+    ];
+    let posicionActual = 0;
+    let $botonRetroceder = document.querySelector('#retroceder');
+    let $botonAvanzar = document.querySelector('#avanzar');
+    let $imagen = document.querySelector('#imagen');
+    // Funciones
+
+    /**
+     * Funcion que cambia la foto en la siguiente posicion
+     */
+    function pasarFoto() {
+        if(posicionActual >= IMAGENES.length - 1) {
+            posicionActual = 0;
+        } else {
+            posicionActual++;
+        }
+        renderizarImagen();
+    }
+    /**
+     * Funcion que cambia la foto en la anterior posicion
+     */
+    function retrocederFoto() {
+        if(posicionActual <= 0) {
+            posicionActual = IMAGENES.length - 1;
+        } else {
+            posicionActual--;
+        }
+        renderizarImagen();
+    }
+    /**
+     * Funcion que actualiza la imagen de imagen dependiendo de posicionActual
+     */
+    function renderizarImagen () {
+        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+    }
+    // Eventos
+    $botonAvanzar.addEventListener('click', pasarFoto);
+    $botonRetroceder.addEventListener('click', retrocederFoto);
+    // Iniciar
+    renderizarImagen();
+} 
